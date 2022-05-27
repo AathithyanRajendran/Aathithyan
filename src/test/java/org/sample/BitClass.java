b2c25ed23d0548c5e266a29692e7f7b33ed82b42
AathithyanRajendran <aathi470@gmail.com>
2022-05-27 11:00:13 +0530

    aathi code

---
+++ src/test/java/org/sample/BitClass.java
@@ -0,0 +1,20 @@
+package org.sample;
+
+public class BitClass {
+	
+		private void method1() {
+			System.out.println("method 1");
+
+		}
+		private void method2() {
+			System.out.println("method 2");
+
+		}
+		private void method3() {
+			System.out.println("method 3");
+		}
+}
+		
+	
+
+
